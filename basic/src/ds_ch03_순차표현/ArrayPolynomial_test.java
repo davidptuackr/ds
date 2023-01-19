9665fa200c208d022921ed635bca169d14ed3138
david_bowman <2017121042@ptu.ac.kr>
2023-01-19 19:03:24 +0900

    03장 예제 완료

--- basic/src/ds_ch03_순차표현/ArrayPolynomial_test.java
+++ basic/src/ds_ch03_순차표현/ArrayPolynomial_test.java
@@ -42,6 +42,25 @@ package ds_ch03_순차표현;
             2. loc_p1++
         4.2 p1[loc_p1]의 차수 > p2[loc_p2]의 차수일 경우
             1. 4.2 반대로
+
+ 3.
+    p1, p2가 있을 때
+    크기가 (2) * (p1 길이 * p2 길이)인 배열 생성, sizeof_p3=p1 * p2
+    다음을 p1 길이만큼 반복: i
+        다음을 p2 길이만큼 반복: j
+            곱셈결과[i*p2길이+j][0]: 계수 곱
+            곱셈결과[i*p2길이+j][1]: 차수 합
+    다음을 곱셈 결과 길이만큼 반복: i
+        현재 칸을 기억
+        다음을 i to 곱셈결과 길이까지 반복: j
+            현재 칸의 차수 == 비교하려는 칸 차수일 경우 현재 칸의 계수 = 현재 칸의 계수 + 비교 칸의 계수
+            비교하려는 칸의 차수를 -1로 설정
+            sizeof_p3 1 감소
+    길이가 sizeof_p3인 다항식 p3 생성
+    오프셋 = 0
+    다음을 곱셈 결과 길이만큼 반복: i
+        곱셈결과의 차수가 -1이 아니라면 p3[i-오프셋]에 추가
+        -1이면 추가하지 않음 & 오프셋 + 1
  */
 import java.util.Scanner;
 
@@ -90,39 +109,77 @@ class ArrayPolynomial {
         }
 
         ArrayPolynomial p3 = new ArrayPolynomial(sizeof_p);
-        int loc_p1 = 0, loc_p2 = 0;
-        int longer = Math.min(p1.p.length, p2.p.length);
+        int loc_p1 = 0, loc_p2 = 0, loc_p3 = 0;
 
-        for (int i = 0; i < sizeof_p - longer; i++) {
+        while ((loc_p1 < p1.p.length) && (loc_p2 < p2.p.length)) {
             if (p1.p[loc_p1][1] == p2.p[loc_p2][1]) {
-                p3.p[i][0] = p1.p[loc_p1][0] + p2.p[loc_p2][0];
-                p3.p[i][1] = p1.p[loc_p1][1];
-                loc_p1++; loc_p2++;
-            } else if (p1.p[loc_p1][1] >= p2.p[loc_p2][1]) {
-                p3.p[i][0] = p1.p[loc_p1][0];
-                p3.p[i][1] = p1.p[loc_p1][1];
-                loc_p1++;
+                p3.p[loc_p3][0] = p1.p[loc_p1][0] + p2.p[loc_p2][0];
+                p3.p[loc_p3][1] = p1.p[loc_p1][1];
+                loc_p1++; loc_p2++; loc_p3++;
+            } else if (p1.p[loc_p1][1] > p2.p[loc_p2][1]) {
+                p3.p[loc_p3][0] = p1.p[loc_p1][0];
+                p3.p[loc_p3][1] = p1.p[loc_p1][1];
+                loc_p1++; loc_p3++;
             } else {
-                p3.p[i][0] = p2.p[loc_p2][0];
-                p3.p[i][1] = p2.p[loc_p2][1];
-                loc_p2++;
+                p3.p[loc_p3][0] = p2.p[loc_p2][0];
+                p3.p[loc_p3][1] = p2.p[loc_p2][1];
+                loc_p2++; loc_p3++;
             }
         }
 
         if (loc_p1 < p1.p.length) {
-            for (int i = loc_p2; i < sizeof_p; i++) {
+            for (int i = loc_p3; i < sizeof_p; i++) {
                 p3.p[i][0] = p1.p[loc_p1][0];
                 p3.p[i][1] = p1.p[loc_p1++][1];
             }
         }
         if (loc_p2 < p2.p.length) {
-            for (int i = loc_p1; i < sizeof_p; i++) {
-                p3.p[i][0] = p1.p[loc_p2][0];
-                p3.p[i][1] = p1.p[loc_p2++][1];
+            for (int i = loc_p3; i < sizeof_p; i++) {
+                p3.p[i][0] = p2.p[loc_p2][0];
+                p3.p[i][1] = p2.p[loc_p2++][1];
             }
         }
 
         return p3;
+    }
+
+    static ArrayPolynomial polyMult(ArrayPolynomial p1, ArrayPolynomial p2) {
+        int[][] mul_t = new int[p1.p.length * p2.p.length][2];
+        int sizeof_p3= p1.p.length * p2.p.length;
+        int offset = 0;
+        ArrayPolynomial p3;
+
+        for (int i = 0; i < p1.p.length; i++) {
+            for (int j = 0; j < p2.p.length; j++) {
+                mul_t[i*p2.p.length + j][0] = p1.p[i][0] * p2.p[j][0];
+                mul_t[i*p2.p.length + j][1] = p1.p[i][1] + p2.p[j][1];
+            }
+        }
+
+        for (int i = 0; i < mul_t.length; i++) {
+            for (int j = i+1; j < mul_t.length; j++) {
+                if (mul_t[i][1] == mul_t[j][1]) {
+                    mul_t[i][0] = mul_t[i][0] + mul_t[j][0];
+                    mul_t[j][1] = -1;
+                    sizeof_p3--;
+                }
+            }
+        }
+
+        p3 = new ArrayPolynomial(sizeof_p3);
+
+        for (int i = 0; i < mul_t.length; i++) {
+            if (mul_t[i][1] != -1) {
+                p3.p[i-offset][0] = mul_t[i][0];
+                p3.p[i-offset][1] = mul_t[i][1];
+            } else offset++;
+        }
+        return p3;
+    }
+
+    int polyEval(int x, int i) {
+        if (i == this.p.length) return 0;
+        else return (int) (this.p[i][0] * Math.pow(x, this.p[i][1])) + polyEval(x, i+1);
     }
 }
 
@@ -130,15 +187,24 @@ public class ArrayPolynomial_test {
 
     public static void main(String[] args) {
 
-        ArrayPolynomial p1 = new ArrayPolynomial();
-        ArrayPolynomial p2 = new ArrayPolynomial();
+        ArrayPolynomial p1 = new ArrayPolynomial(2);
+        ArrayPolynomial p2 = new ArrayPolynomial(3);
         ArrayPolynomial p3;
-        p1.read();
-        p2.read();
+        p1.p[0][0] = 2; p1.p[0][1] = 7;
+        p1.p[1][0] = 10; p1.p[1][1] = 2;
+        p2.p[0][0] = 10; p2.p[0][1] = 7;
+        p2.p[1][0] = 20; p2.p[1][1] = 5;
+        p2.p[2][0] = 30; p2.p[2][1] = 2;
+
         p1.print();
         p2.print();
 
         p3 = ArrayPolynomial.polyAdd(p1, p2);
         p3.print();
+
+        p3 = ArrayPolynomial.polyMult(p1, p2);
+        p3.print();
+
+        System.out.println(p3.polyEval(1, 0));
     }
 }
