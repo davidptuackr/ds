321854314880226bd2e10907b4872222649c84d2
david_bowman <2017121042@ptu.ac.kr>
2023-04-04 18:02:35 +0900

    ~ List_Binary_Tree non-rec pre order 구현 완료

--- basic/src/ds_ch07_트리/DS_ch07.java
+++ basic/src/ds_ch07_트리/DS_ch07.java
@@ -52,7 +52,6 @@ interface Binary_Tree {
 class List_Binary_Tree implements Binary_Tree {
 
     Object[] data;
-    int cursor = 1;
 
     public List_Binary_Tree(int h) {
         data = new Object[(int) Math.pow(2, h + 1)];
@@ -240,6 +239,7 @@ class List_Binary_Tree implements Binary_Tree {
 
     @Override
     public void rec_pre_order() {
+        rec_pre_order(1);
     }
 
     @Override
@@ -252,8 +252,13 @@ class List_Binary_Tree implements Binary_Tree {
 
     }
 
-    public void rec_pre_order(int cursor) {
-        if ((cursor == 0) || (cursor > data.length) || (data[cursor] == null)) {
+    private void rec_pre_order(int cursor) {
+        if (cursor == 0) {
+            System.out.println();
+            return;
+        }
+
+        if ((cursor > data.length) || (data[cursor] == null)) {
             return;
         }
 
@@ -313,7 +318,38 @@ class List_Binary_Tree implements Binary_Tree {
 
     @Override
     public void in_order() {
+        if (is_empty()) {
+            System.out.println("EMPTY TREE");
+            return;
+        }
+        boolean[] mark = new boolean[data.length];
+        int p = 1;
+        /*
+        루트 방문 조건
+            p*2 > data.length일 때
+            왼쪽을 다 돌았을 때
+            좌측 서브트리가 없을 때
+        왼쪽 이동 조건: 왼쪽이 null이 아닐 것 + 현재 위치가 리프가 아닐 것 + 방문하지 않았을 것
+        오른쪽 이동 조건: 루트를 방문했을 것
 
+         */
+        while (p != 0) {
+            if ((p*2 >= data.length) || (mark[p*2] && !mark[p*2+1]) || (!mark[p*2+1] && (data[p*2] == null))) {
+                System.out.print(data[p] + " ");
+                mark[p] = true;
+            }
+
+            if ((p*2 < data.length) && (data[p*2] != null) && (!mark[p*2])) {
+                p *= 2;
+            }
+            else if ((p*2+1 < data.length) && (data[p*2+1] != null) && (!mark[p*2+1])) {
+                p = p * 2 + 1;
+            }
+            else {
+                p /= 2;
+            }
+        }
+        System.out.println();
     }
 
     @Override
@@ -364,52 +400,66 @@ public class DS_ch07 {
         return null;
     }
 
-    static void list_bt_test() {
-        List_Binary_Tree bt = new List_Binary_Tree(3);
-
-        System.out.println(bt.is_empty());
-
-        bt.insert("Alpha", 1);
-        bt.insert("Sig", 2);
-        bt.insert("Nut", 3);
-        bt.insert("Z", 5);
-        bt.insert("Aki", 6);
-        bt.insert("Juan", 7);
-        bt.insert("Sq", 4);
-        bt.insert("Wood", 9);
-
-        System.out.println(bt.is_empty());
-
+    static void list_bt_test(List_Binary_Tree bt) {
+        
+        System.out.println("IS EMPTY TEST: EMPTY? >>> " + bt.is_empty());
+        System.out.println("NON RECURSIVE PRE ORDER TEST");
         bt.pre_order();
 
+        System.out.println("INSERTION TEST");
         bt.insert("Wood", 9);
-
         bt.insert("Sq", 4);
         bt.insert("Wood", 9);
-
+        System.out.println("AFTER INSERTION");
         bt.pre_order();
 
+        System.out.println("CPY TEST");
         List_Binary_Tree cpy = (List_Binary_Tree) bt.copy();
         List_Binary_Tree bt2 = new List_Binary_Tree(4);
         bt2.insert("Alpha", 1);
         bt2.insert("Sig", 2);
         bt2.insert("Nut", 3);
         bt2.insert("Quan", 5);
-
         System.out.println("IS EQUAL bt, cpy? >>> " + bt.is_equal(cpy));
-        System.out.println("IS EQUAL bt, cpy? >>> " + bt.is_equal(bt2));
+        System.out.println("IS EQUAL bt, bt2? >>> " + bt.is_equal(bt2));
 
+        System.out.println("DELETION TEST");
         bt.delete("Z");
         bt.delete("Nut");
         bt.delete("Os");
+        System.out.println("AFTER DELETE");
         bt.pre_order();
 
-        bt.rec_pre_order(1);
+        System.out.println("RECURSIVE PRE ORDER TEST");
+        bt.rec_pre_order();
+
+        bt.in_order();
     }
 
     public static void main(String[] a) {
 
-        list_bt_test();
+        /*List_Binary_Tree bt1 = new List_Binary_Tree(3);
+        bt1.insert("Alpha", 1);
+        bt1.insert("Sig", 2);
+        bt1.insert("Nut", 3);
+        bt1.insert("Z", 5);
+        bt1.insert("Aki", 6);
+        bt1.insert("Juan", 7);
+        bt1.insert("Sq", 4);
+        bt1.insert("Wood", 9);
+        list_bt_test(bt1);*/
+
+        List_Binary_Tree bt2 = new List_Binary_Tree(3);
+        bt2.insert("A");
+        bt2.insert("B");
+        bt2.insert("C");
+        bt2.insert("D");
+        bt2.insert("E", 6);
+        bt2.insert("F", 7);
+        bt2.insert("G", 8);
+        bt2.insert("H", 15);
+        bt2.in_order();
+
     }
 
 }
