f58802e37811e2a72c11afcffb8f31e082cbb9ba
david_bowman <2017121042@ptu.ac.kr>
2023-05-30 18:50:44 +0900

    Link_BST 완료, List_BST 개시: List_BST.insert 구현 중

--- basic/src/ds_ch08_BST/DS_ch08.java
+++ basic/src/ds_ch08_BST/DS_ch08.java
@@ -71,16 +71,112 @@ interface BST {
     String describe();
 }
 
-abstract class List_BST implements BST {
+class List_BST implements BST {
+
+    private class Node {
+
+        int key;
+        Object data;
+
+        public Node(int key, Object data) {
+            this.key = key;
+            this.data = data;
+        }
+
+    }
+
+    int h;
+    int cnt;
+    private Node root;
+    private Node[] tree;
+
+    public List_BST(int max_h) {
+        this.tree = new Node[(int) Math.pow(2, max_h)];
+        this.root = tree[0];
+        this.cnt = 0;
+        this.h = 0;
+    }
 
     @Override
     public boolean empty() {
-        return false;
+        return this.tree[0] == null;
     }
 
     @Override
     public void insert(int key, Object data_in) {
 
+        /*
+        순차 표현으로 구현한 이진 탐색 트리에서의 삽입
+
+            과정
+                - 빈 트리면 루트에 삽입하고 끝낸다
+                - 아니면 쭉 돌아다니면서 들어갈 수 있는 자리를 찾아본다
+                - 자리를 찾아냈다면 (1. 삽입, 2. 원소 수 증가, 3. 종료) 순으로 작업한다
+                - 자리가 없다면 (1. 배열 확장, 2. 삽입 재시도, 3. 원소 수 증가, 4. 종료) 순으로 작업한다
+                - 이미 있는 원소는 (1. 삽입 불가를 알림, 2. 종료) 순으로 작업한다
+
+            상세 (ADL)
+                삽입(키, 데이터) {
+                    if empty tree :
+                        루트(==tree[0]) = (키, 데이터)
+                        삽입 종료
+
+                    자리찾기 과정
+                        1. 루트랑 비교했을 때
+                            키가 작다면 왼쪽 자식과 다시 비교
+                            키가 크다면 오른쪽 자식과 다시 비교
+                        2. 비교 대상이
+                            null이 아니면 null이 아닐 때까지 이동 + 비교
+                            null이면 해당 위치에 삽입
+                        3. 더 이상 비교할 수가 없다면 == 자리가 없다면
+                            배열 확장 후 삽입 재시도
+
+                    자리를 잡고 나면 원소 수 +1
+                    종료
+                }
+         */
+
+
+        if (this.empty()) {
+            tree[0] = new Node(key, data_in);
+            cnt++;
+            return;
+        }
+
+        int i_cmp = 0;
+
+        while (i_cmp < tree.length) {
+            if (tree[i_cmp].key == key) {
+                System.out.printf("UNABLE TO INSERT (%d, %s). KEY %d IS ALREADY EXISTS\n", key, data_in, key);
+                return;
+            }
+
+            if (tree[i_cmp] == null) {
+                tree[i_cmp] = new Node(key, data_in);
+                return;
+            }
+
+            i_cmp = (tree[i_cmp].key > key) ? (i_cmp + 1) * 2 - 1 : (i_cmp + 1) * 2;
+        }
+
+        int i_iter = 1;
+        Node[] tree_exp = new Node[tree.length * 2];
+        tree_exp = exp_routine(tree_exp, i_iter);
+
+        tree = tree_exp;
+        tree[i_cmp] = new Node(key, data_in);
+    }
+    private Node[] exp_routine(Node[] tree_exp, int i_iter) {
+
+        if ((i_iter * 2 + 1 < tree.length) && (tree_exp[i_iter * 2 + 1] != null)) {
+            tree_exp = exp_routine(tree_exp, i_iter * 2 + 1);
+        }
+        tree_exp[i_iter] = new Node(tree[i_iter].key, tree[i_iter].data);
+        if ((i_iter * 2 + 1 < tree.length) && (tree_exp[(i_iter + 1) * 2] != null)) {
+            tree_exp = exp_routine(tree_exp, i_iter * 2 + 1);
+        }
+
+        return tree_exp;
     }
 
     @Override
@@ -105,6 +201,11 @@ abstract class List_BST implements BST {
 
     @Override
     public BST reshape() {
+        return null;
+    }
+
+    @Override
+    public String describe() {
         return null;
     }
 }
@@ -512,19 +613,10 @@ class Link_BST implements BST {
 
     public String describe() {
         /*
-        과정
-        1. 각 레벨에서 가장 큰 원소 탐색
-            1.1 Node 리스트 활용
-            1.2 가장 오른쪽에 있는 원소 탐색
-            1.3 오른쪽 끝의 자식이 둘 다 null 일 경우 백트레킹
-                백트레킹 과정
-                1.3.1 부모의 왼쪽 자식의 오른쪽 노드 탐색
-                1.3.2 부모의 왼쪽 자식의 오른쪽이 null 이면 왼쪽 자식의 왼쪽 탐색
-                1.3.3 왼쪽 자식도 null 이면 부모의 부모 백트레킹
-                1.3.4 내려가는 횟수 1회 증가
-                1.3.5 루트까지 돌아왔을 경우 (배열 마지막 원소 인덱스 + 1)회만큼 오른쪽으로 이동
-        2. 루트를 큐에 삽입
-        3. 아까 peek 한 원소가 해당 레벨 최대 노드였다면 레벨 정보를 sb에 추가 삽입
+        설정
+            q: 레벨 순서로 뽑아내기 위한 큐. 초기에 루트 삽입
+            is_lv_first: true일 경우 peek 한 노드가 해당 레벨의 첫 원소라는 의미. 레벨 정보 추가 목적.
+            lv_first: 레벨 첫 원소의 키를 저장
          */
 
         StringBuilder sb = new StringBuilder();
@@ -534,47 +626,6 @@ class Link_BST implements BST {
         int lv_first = this.root.key;
         int lv = 1;
         Node peeked;
-
-        /*while (!q.isEmpty()) {
-            if ((cnt) == Math.pow(2, lv-1)) {
-                sb.append(String.format("\nLEVEL %d: ", lv++));
-            }
-            else {
-                sb.append("\t, ");
-            }
-            cnt++;
-            if (q.peek() != null) {
-                polled = q.poll();
-            }
-            else {
-                q.remove();
-                continue;
-            }
-            sb.append(polled.data);
-            q.add(polled.left);
-            q.add(polled.right);
-        }*/
-        /*
-        while (!q.isEmpty()) {
-            if (q.peek().key <= dq.peekFirst().key) {
-                sb.append(String.format("\nLEVEL %d: %s ", lv++, q.peek().data));
-            }
-            else {
-                sb.append(q.peek().data);
-                sb.append(" ");
-            }
-            q.remove();
-
-            if (peeked.left != null) {
-                q.add(peeked.left);
-            }
-            if (peeked.right != null) {
-                q.add(peeked.right);
-            }
-        }
-
-        return sb.toString();
-    }*/
 
         while (!q.isEmpty()) {
             if (lv_first == q.peek().key) {
@@ -605,54 +656,6 @@ class Link_BST implements BST {
 
         return sb.toString();
     }
-    /*
-    private class LV_maxes {
-
-        Node front;
-        Node tail;
-        Node cursor;
-        int lv;
-
-        public LV_maxes() {
-            this.front = new Node(root.key, root, null, null);
-            this.tail = front;
-            this.cursor = front;
-            this.lv = 1;
-        }
-    }
-    private LV_maxes get_lv_maxes() {
-
-        LV_maxes lv_maxes = new LV_maxes();
-
-        return lv_maxes_routine(lv_maxes, lv_maxes.tail, lv_maxes.cursor, root, lv_maxes.lv);
-    }
-
-    private LV_maxes lv_maxes_routine(LV_maxes lv_maxes, Node t, Node c, Node data, int loc) {
-
-        if (data.right != null) {
-            Node p = new Node(data.right.key, data.right, t, null);
-            lv_maxes.tail.right = p;
-            lv_maxes.tail = p;
-            lv_maxes.cursor = p;
-            lv_maxes.lv++;
-            return lv_maxes_routine(lv_maxes, t.right, c.right, data.right, lv_maxes.lv);
-        }
-        else if (data.left != null) {
-            Node p = new Node(data.left.key, data.left, t, null);
-            lv_maxes.tail.right = p;
-            lv_maxes.tail = p;
-            lv_maxes.cursor = p;
-            lv_maxes.lv++;
-            return lv_maxes_routine(lv_maxes, t.right, c.right, data.left, lv_maxes.lv);
-        }
-        else {
-            lv_maxes.cursor = t.left;
-
-        }
-
-        return null;
-    }
-    */
 }
 
 public class DS_ch08 {
