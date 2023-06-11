b32dfd97f76dadf730dbc6bf0206d8dda989bbe5
david_bowman <2017121042@ptu.ac.kr>
2023-06-11 11:36:55 +0900

    List_BST.b_insert 재배치 과정 리팩토링: 중복 코드 제거

--- basic/src/ds_ch08_BST/DS_ch08.java
+++ basic/src/ds_ch08_BST/DS_ch08.java
@@ -650,50 +650,50 @@ class List_BST implements BST {
             }
         }
 
+        /*
+        재배치 과정 리팩토링 (061123)
+
+        1. 중복된 tree[i_loc] = new Node(key, data_in); 제거 (O)
+         */
+
         if (i_bef != -1 && i_aft != -1) {
             if ((tree[i_bef].key < key) && (key < tree[i_aft].key)) {
-                tree[i_loc] = new Node(key, data_in);
+
             }
             else if (key < tree[i_bef].key) {
                 for (int i = i_init ; key < tree[i_inords[i-1]].key; i--) {
                     tree[i_inords[i]] = new Node(tree[i_inords[i-1]].key, tree[i_inords[i-1]].data);
                     i_loc = i_inords[i-1];
                 }
-                tree[i_loc] = new Node(key, data_in);
             }
             else if (tree[i_aft].key < key) {
                 for (int i = i_init ; i_inords[i+1] != -1 && tree[i_inords[i+1]].key < key; i++) {
                     tree[i_inords[i]] = new Node(tree[i_inords[i+1]].key, tree[i_inords[i+1]].data);
                     i_loc = i_inords[i+1];
                 }
-                tree[i_loc] = new Node(key, data_in);
             }
         }
         else if (i_bef == -1) { // 레벨 첫번째인 경우
             if (key < tree[i_aft].key) {
-                tree[i_loc] = new Node(key, data_in);
             }
             else {
                 for (int i = i_init ; tree[i_inords[i+1]].key < key && i_inords[i+1] != -1; i++) {
                     tree[i_inords[i]] = new Node(tree[i_inords[i+1]].key, tree[i_inords[i+1]].data);
                     i_loc = i_inords[i+1];
                 }
-                tree[i_loc] = new Node(key, data_in);
             }
         }
         else { // 레벨 마지막인 경우
             if (tree[i_bef].key < key) {
-                tree[i_loc] = new Node(key, data_in);
             }
             else {
                 for (int i = i_init ; key < tree[i_inords[i-1]].key; i--) {
                     tree[i_inords[i]] = new Node(tree[i_inords[i-1]].key, tree[i_inords[i-1]].data);
                     i_loc = i_inords[i-1];
                 }
-                tree[i_loc] = new Node(key, data_in);
             }
         }
-
+        tree[i_loc] = new Node(key, data_in);
 
     }
 }
